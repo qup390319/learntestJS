52a3f887c7dbb8915034dd15ab577aa0e54aef3c
Snail <40841103@gm.nfu.edu.tw>
2021-08-04 16:01:35 +0800

    Commit Test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.write("hello world")
+// var age = 123;
+// var is_male = true;
+
+// document.write("hello world"+age);
+
+var num1 = prompt("數字1");
+var num2 = prompt("數字2");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+document.write(num1+num2);
+document.write("hi");
